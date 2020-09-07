b891544018cddac883fe6fc7c0cb07a1f2ebe558
Ahmed Shaaban <32022093+AhmedSh3ban@users.noreply.github.com>
2020-09-07 03:29:51 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,187 +1,154 @@
-Config = {}
+var _stopTimer = false;
 
-Config.DrawDistance = 100
-Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
-Config.BuyZoneSize  = {x = 4, y = 4, z = 4}
-Config.Color        = {r = 0, g = 128, b = 255}
-Config.TeamDeathMatchBlip = { x = 2071.08, y = 2761.78, z = 50.28}
-Config.Spectate = {x = 1970.84, y = 2773.39, z = 59.38}
-Config.MapCenter = {x = 2014.73, y = 2784.93, z = 49.11}
-
-Config.Deathmatch = {
-    BlueTeam = {
-        name = "Đội Xanh",
-        color = { r = 0, g = 128, b = 255},
-        game_start_pos = { x = 2028.27, y = 2857.86, z = 50.17},
-        enter_pos = { x = 2069.69, y = 2763.74, z = 49.11},
-        skin = {
-            male = {
-                ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
-                ['torso_1'] = 208,   ['torso_2'] = 0,
-                ['decals_1'] = 0,   ['decals_2'] = 0,
-                ['arms'] = 130,
-                ['pants_1'] = 86,   ['pants_2'] = 0,
-                ['shoes_1'] = 59,   ['shoes_2'] = 0,
-                ['helmet_1'] = 123,  ['helmet_2'] = 0,
-                ['chain_1'] = 0,    ['chain_2'] = 0,
-                ['ears_1'] = 0,     ['ears_2'] = 0,
-                ['bproof_1'] = 16,  ['bproof_2'] = 2
-
-            },
-            female = {
-                ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
-                ['torso_1'] = 212,   ['torso_2'] = 0,
-                ['decals_1'] = 0,   ['decals_2'] = 0,
-                ['arms'] = 44,
-                ['pants_1'] = 89,   ['pants_2'] = 0,
-                ['shoes_1'] = 62,   ['shoes_2'] = 0,
-                ['helmet_1'] = 122,  ['helmet_2'] = 0,
-                ['chain_1'] = 0,    ['chain_2'] = 0,
-                ['ears_1'] = 0,     ['ears_2'] = 0,
-                ['bproof_1'] = 18,  ['bproof_2'] = 2
-            }
+$(function(){
+    window.addEventListener("message", function(event){   
+        if(event.data.type == "show_game_ui") {
+            $('#header').show();
+        } else if (event.data.type == "show_game_scoreboard") {
+            $('#game_scoreboard').toggle();
+        } else if (event.data.type == "update_game_ui") {
+            this.console.log(event.data);
+            mapUI(event.data);
+        } else if (event.data.type == "update_game_ui_win") {
+            mapUI(event.data);
+            showWinTeam(event.data);
+        } else if (event.data.type == "update_game_ui_lose") {
+            mapUI(event.data);
+            showWinTeam(event.data);
+        } else if(event.data.type == "match_start") {
+            startMatch();
+        } else if(event.data.type == "new_round") {
+            $('#who_won').hide();
+            // reset time
+            startMatch();
+        } else if (event.data.type == "endgame") {
+            Speak("Tráº­n Ä‘áº¥u Ä‘Ã£ káº¿t thÃºc!");
+            $('#header').hide();
+            this.location.reload();
+        } else if(event.data.type == "update_game_ui_win_finished") {
+            // mapUI(event.data);
+            this.console.log(event.data);
+            showWinTeam(event.data, true);
+        } else if(event.data.type == "voice_anount") {
+            Anount(event.data.team, event.data.kill);
         }
-    },
-    RedTeam = {
-        name = "Đội Đỏ",
-        color = { r = 255, g = 0, b = 0},
-        game_start_pos = { x = 2016.07, y = 2705.95, z = 49.95},
-        enter_pos = { x = 2071.84, y = 2757.12, z = 49.02},
-        skin = {
-            male = {
-                ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
-                ['torso_1'] = 208,   ['torso_2'] = 21,
-                ['decals_1'] = 0,   ['decals_2'] = 0,
-                ['arms'] = 130,
-                ['pants_1'] = 86,   ['pants_2'] = 21,
-                ['shoes_1'] = 59,   ['shoes_2'] = 23,
-                ['helmet_1'] = 123,  ['helmet_2'] = 0,
-                ['chain_1'] = 0,    ['chain_2'] = 0,
-                ['ears_1'] = 0,     ['ears_2'] = 0,
-                ['bproof_1'] = 16,  ['bproof_2'] = 2
-            },
-            female = {
-                ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
-                ['torso_1'] = 212,   ['torso_2'] = 21,
-                ['decals_1'] = 0,   ['decals_2'] = 0,
-                ['arms'] = 44,
-                ['pants_1'] = 89,   ['pants_2'] = 21,
-                ['shoes_1'] = 62,   ['shoes_2'] = 25,
-                ['helmet_1'] = 122,  ['helmet_2'] = 0,
-                ['chain_1'] = 0,    ['chain_2'] = 0,
-                ['ears_1'] = 0,     ['ears_2'] = 0,
-                ['bproof_1'] = 18,  ['bproof_2'] = 2
-            }
+    });
+});
+
+function mapUI(data)
+{
+    var _blueTeam = data.game_ui.BlueTeam;
+    var _redTeam = data.game_ui.RedTeam;
+    // Blue players
+    var _index = 0;
+    for(var i = 0; i < _blueTeam.player_list.length; i++) {
+        if (_blueTeam.player_list[i] != undefined) {
+            _index++;
+            $(`.blue-team[data-player-id="${_index}"] .bar_username`).text(_blueTeam.player_list[i].name);
+            $(`.blue-team[data-player-id="${_index}"] .kills`).eq(1).text(_blueTeam.player_list[i].kill);
+            $(`.blue-team[data-player-id="${_index}"] .deaths`).eq(1).text(_blueTeam.player_list[i].death);
+            $(`.blue-team[data-player-id="${_index}"]`).show();
         }
     }
-}
-
--- Config.Uniforms = 
-
-Config.BuyMenu = {
-    Knife = {
-        label = "0. Cận chiến",
-        list = {
-            {
-                label = "Búa",
-                key = "WEAPON_BATTLEAXE"
-            },
-            {
-                label = "Mã tấu",
-                key = "WEAPON_MACHETE"
-            },
-            {
-                label = "Dao bấm",
-                key = "WEAPON_SWITCHBLADE"
-            }
+    // Score
+    $('.blue_score').text(_blueTeam.score);
+    // Red players
+    var _index = 0;
+    for(var i = 0; i < _redTeam.player_list.length; i++) {
+        if (_redTeam.player_list[i] != undefined) {
+            _index++;
+            $(`.red-team[data-player-id="${_index}"] .bar_username`).text(_redTeam.player_list[i].name);
+            $(`.red-team[data-player-id="${_index}"] .kills`).eq(1).text(_redTeam.player_list[i].kill);
+            $(`.red-team[data-player-id="${_index}"] .deaths`).eq(1).text(_redTeam.player_list[i].death);
+            $(`.red-team[data-player-id="${_index}"]`).show();
         }
-    },
-    Pistol = {
-        label = "1. Súng lục",
-        list = {
-            {
-                label = "Pistol",
-                key = "WEAPON_PISTOL"
-            },
-            {
-                label = "AP Pistol",
-                key = "WEAPON_APPISTOL"
-            },
-            {
-                label = "Revolver",
-                key = "WEAPON_REVOLVER"
-            }
-        }
-    },
-    ShortGun = {
-        label = "2. Shortgun",
-        list = {
-            {
-                label = "Sawnoff Shotgun",
-                key = "WEAPON_SAWNOFFSHOTGUN"
-            },
-            {
-                label = "Pump shotgun",
-                key = "WEAPON_PUMPSHOTGUN"
-            },
-            {
-                label = "Heavy Shotgun",
-                key = "WEAPON_HEAVYSHOTGUN"
-            },
-            {
-                label = "Musket",
-                key = "WEAPON_MUSKET"
-            }
-            
-        }
-    },
-    SMG = {
-        label = "3. Tiểu liên",
-        list = {
-            {
-                label = "Uzi",
-                key = "WEAPON_MICROSMG"
-            },
-            {
-                label = "MP5",
-                key = "WEAPON_SMG"
-            }
-        }
-    },
-    Rifle = {
-        label = "4. Trung liên",
-        list = {
-            {
-                label = "M4",
-                key = "WEAPON_CARBINERIFLE"
-            },
-            {
-                label = "AK",
-                key = "WEAPON_ASSAULTRIFLE"
-            },
-            {
-                label = "AWM",
-                key = "WEAPON_SNIPERRIFLE"
-            }
-        }
-    },
-    Genade = {
-        label = "5. Lưu đạn",
-        list = {
-            {
-                label = "Genade",
-                key = "WEAPON_GRENADE"
-            },
-            {
-                label = "Molotov",
-                key = "WEAPON_MOLOTOV"
-            },
-            {
-                label = "Lưu đạn khói",
-                key = "WEAPON_SMOKEGRENADE"
-            }
-            
-        }
-        
     }
+    // Score
+    $('.red_score').text(_redTeam.score);
+    //
 }
+
+function showWinTeam(data, finished)
+{
+    var _textSpeak = "";
+    if(data.win_team == "RedTeam") {
+        $('#winning_team').html(`<span class="red">Red Team</span>`);
+        _textSpeak = "The red team has won!";
+        // Speak("Äá»™i Ä‘á» Ä‘Ã£ chiáº¿n tháº¯ng!");
+    } else {
+        $('#winning_team').html(`<span class="blue">Blue Team</span>`);
+        _textSpeak = "The blue team has won!";
+        // Speak("Äá»™i xanh Ä‘Ã£ chiáº¿n tháº¯ng!");
+    }
+    $('#who_won').fadeIn();
+    if(finished) {
+        $('.div_info').html(`The match will end in <span class="red" id="remain_time">15</span> seconds more!`);
+        _textSpeak += " The match will end in 15 seconds!";
+    }
+    Speak(_textSpeak);
+
+    _stopTimer = true;
+    var _sec = 15;
+    var _interVal = setInterval(function () {
+        $('#remain_time').text(_sec); 
+        _sec--;
+
+        if(_sec < 0) {
+            clearInterval(_interVal);
+        }
+    }, 1000);
+    
+}
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    var _interVal = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+        if(_stopTimer) {
+            clearInterval(_interVal);
+        }
+    }, 1000);
+}
+
+function startMatch() {
+    _stopTimer = false;
+    $('#who_won').hide();
+    var fiveMinutes = 60 * 3,
+        display = $('#time_counter');
+    startTimer(fiveMinutes, display);
+    Speak("The match has started!");
+}
+
+function Speak(text) {
+    $('#speak_ai').attr("src", "https://translate.google.com/translate_tts?ie=UTF-8&q="+encodeURI(text)+"&tl=en-us&total=1&idx=0&client=tw-ob");
+    // $('#speak_ai').play();
+    document.getElementById('speak_ai').play();
+}
+
+function Anount(_team, _kill) {
+    $('#anount_ai').attr("src", _anount[_team][_kill]);
+    document.getElementById('anount_ai').play();
+}
+
+var _anount = [];
+_anount["allied"] = [];
+_anount["allied"]["double"] = "sounds/DoubleKill.mp3";
+_anount["allied"]["triple"] = "sounds/TripleKill.mp3";
+_anount["allied"]["quadra"] = "sounds/QuadraKill.mp3";
+_anount["allied"]["penta"] = "sounds/PentaKill.mp3";
+_anount["enemy"] = [];
+_anount["enemy"]["double"] = "sounds/EnemyDoubleKill.mp3";
+_anount["enemy"]["triple"] = "sounds/EnemyTripleKill.mp3";
+_anount["enemy"]["quadra"] = "sounds/EnemyQuadraKill.mp3";
+_anount["enemy"]["penta"] = "sounds/EnemyPentaKill.mp3";
+// console.log(_anount);
